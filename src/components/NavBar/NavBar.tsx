707a49f9ecee737c3fac49924c2ba8d645c615b5
Jules <67839718+heyjulesb@users.noreply.github.com>
2022-07-25 23:19:38 +0100

    feat(navbar): using HashLink to scroll to element matching hashfragment

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,16 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import './NavBar.css';
 
 export const NavBar: FC = () => {
   return (
     <nav className='navBar'>
       <Link className='navLink' to="/jules-website"> Home </Link>
-      <Link className='navLink' to="#about"> About </Link>
+      <HashLink smooth className='navLink' to="/jules-website#about"> About </HashLink>
       <Link className='navLink' to="https://medium.com/@heyjulesb" target="_blank" rel="noreferrer"> Blog </Link>
       <Link className='navLink' to="/jules-website/portfolio"> Portfolio </Link>
-      <Link className='navLink' to="#contact"> Contact </Link>
+      <HashLink smooth className='navLink' to="#contact"> Contact </HashLink>
     </nav>
   )
 };
